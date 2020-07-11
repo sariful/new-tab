8f71bd856632319debc3390a7f04c10fe6ad5d65
Sariful Islam <sarifulislam.molla@gmail.com>
2020-07-11 16:12:40 +0530

    updated design

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,13 @@ var zubizi = {
     greetUser: function() {
         var hour = moment().format('H');
         if (hour < 12) {
-            $('.current_greet').text('Good Morning');
+            $('.current_greet').text('Good Morning ');
         } else if (hour == 12) {
-            $('.current_greet').text('Good Noon');
+            $('.current_greet').text('Good Noon ');
         } else if (hour < 17) {
-            $('.current_greet').text('Good Afternoon');
+            $('.current_greet').text('Good Afternoon ');
         } else {
-            $('.current_greet').text('Good Evening');
+            $('.current_greet').text('Good Evening ');
         }
     }
 };
@@ -26,7 +26,7 @@ setInterval(function() {
     if (localStorage.greet_name) {
         greet_name = localStorage.greet_name;
     } else {
-        greet_name = "Your Name";
+        greet_name = "<span class='small'>Double click to edit your name<small>";
     }
     $('.greet_name').text(greet_name);
 
